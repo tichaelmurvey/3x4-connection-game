1c9a7c76e53e8e3e10df6d942cbf6fe617ecc43c
michael <Michaelturvey97@gmail.com>
2025-04-19 19:09:12 +1000

    initial commit

---
+++ src/game/grid/Draggable.tsx
@@ -0,0 +1,17 @@
+import { useDraggable } from '@dnd-kit/core';
+
+function Draggable(props: React.PropsWithChildren) {
+  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+    id: 'draggable',
+  });
+  const style = transform ? {
+    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+  } : undefined;
+
+  
+  return (
+    <button ref={setNodeRef} style={style} {...listeners} {...attributes}>
+      {props.children}
+    </button>
+  );
+}
---
+++ src/game/grid/Droppable.tsx
@@ -0,0 +1,17 @@
+import { useDroppable } from '@dnd-kit/core';
+
+function Droppable(props : React.PropsWithChildren) {
+  const {isOver, setNodeRef} = useDroppable({
+    id: 'droppable',
+  });
+  const style = {
+    color: isOver ? 'green' : undefined,
+  };
+  
+  
+  return (
+    <div ref={setNodeRef} style={style}>
+      {props.children}
+    </div>
+  );
+}
