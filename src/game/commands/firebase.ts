ce43b224d7af5e47b070e40cbf7d0728a06d310e
michael <Michaelturvey97@gmail.com>
2025-05-04 12:55:14 +1000

    firebase removed

---
+++ src/game/commands/firebase.ts
@@ -0,0 +1,30 @@
+import { PuzzleSolution } from "@/game/model/model";
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import {
+	collection,
+	getDocs,
+	getFirestore
+} from "firebase/firestore";
+
+const firebaseConfig = {
+	//your config here
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getFirestore(app);
+
+export async function getAllPuzzles() {
+	const puzzleResponse = await getDocs(collection(database, "puzzles"));
+	const puzzleDataWithoutGroups = puzzleResponse.docs.map(doc => doc.data());
+	//console.log("puzzles without groups", puzzleDataWithoutGroups);
+	const groupFetchers = puzzleResponse.docs.map(puzzle => getDocs(collection(database, "puzzles", puzzle.id, "groups")))
+	const groupsResponse = await Promise.all(groupFetchers)
+	const groups = groupsResponse.map(groupResponse => groupResponse.docs.map(doc => doc.data()));
+	//console.log("groups", groups)
+	const groupsAsArrays = groups.map(groupset => groupset.map(groupobj => [groupobj[0], groupobj[1]]))
+	const puzzles = puzzleDataWithoutGroups.map((puzzle, index) => ({...puzzle, groups: groupsAsArrays[index]}))
+	//console.log("puzzles", puzzles)
+	return puzzles as PuzzleSolution[];
+}
